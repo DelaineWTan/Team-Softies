cc937bfe776fd31b07c8800b67264a27472454eb
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2023-11-09 22:33:59 -0800

    added acceptance test for CombatEventTest

--- test_acceptance.py
+++ test_acceptance.py
@@ -1,4 +1,9 @@
 import unittest
+from io import StringIO
+from unittest.mock import patch
+
+from main import UserMenu
+
 
 class MainMenuTest(unittest.TestCase):
     @classmethod
@@ -50,24 +55,72 @@ class EditorMenuTest(unittest.TestCase):
 
 
 class CombatEventTest(unittest.TestCase):
-    def test_select_attack_option(self):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._menu = UserMenu()
+
+    @classmethod
+    def tearDownClass(cls):
+        # cls._menu.display_main_menu()
+        pass
+
+    @patch('builtins.input', side_effect=['1'])
+    def test_select_attack_option(self, mock_input):
         # Test combat event: Selecting "Attack" option
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self._menu.run_combat_event()
         # Assertions go here
+        printed_output = mock_stdout.getvalue().strip()
+        expected_output = ("You are fighting a Level 1 Goblin!\n"
+                           "1. Attack\n2. Defend\n3. Use Item\n4. Flee\n"
+                           # "Enter your choice (1-4):\n"
+                           "You attacked the Level 1 Goblin for 5 damage!\n"
+                           "Level 1 Goblin died!")
+        self.assertEqual(printed_output, expected_output)
         pass
 
+    @patch('builtins.input', side_effect=['2'])
     def test_select_defend_option(self):
-        # Test combat event: Selecting "Defend" option
+        # Test combat event: Selecting "Attack" option
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self._menu.run_combat_event()
         # Assertions go here
+        printed_output = mock_stdout.getvalue().strip()
+        expected_output = ("You are fighting a Level 1 Goblin!\n"
+                           "1. Attack\n2. Defend\n3. Use Item\n4. Flee\n"
+                           # "Enter your choice (1-4):\n"
+                           "You defended yourself!\n"
+                           "Level 1 Goblin hit you for 1 damage!")
+        self.assertEqual(printed_output, expected_output)
         pass
 
+    @patch('builtins.input', side_effect=['3'])
     def test_select_item_option(self):
-        # Test combat event: Selecting "Item" option
+        # Test combat event: Selecting "Attack" option
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self._menu.run_combat_event()
         # Assertions go here
+        printed_output = mock_stdout.getvalue().strip()
+        expected_output = ("You are fighting a Level 1 Goblin!\n"
+                           "1. Attack\n2. Defend\n3. Use Item\n4. Flee\n"
+                           # "Enter your choice (1-4):\n"
+                           "You used a potion and healed 1 hp!")
+        self.assertEqual(printed_output, expected_output)
         pass
 
+    @patch('builtins.input', side_effect=['4'])
     def test_select_flee_option(self):
-        # Test combat event: Selecting "Flee" option
+        # Test combat event: Selecting "Attack" option
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self._menu.run_combat_event()
         # Assertions go here
+        printed_output = mock_stdout.getvalue().strip()
+        expected_output = ("You are fighting a Level 1 Goblin!\n"
+                           "1. Attack\n2. Defend\n3. Use Item\n4. Flee\n"
+                           # "Enter your choice (1-4):\n"
+                           "You fled successfully!\n")
+        self.assertEqual(printed_output, expected_output)
         pass
 
 
