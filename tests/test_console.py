235b747493b980ca04278342ec2e79b9b1af5ec3
Delaine <delainewtan@gmail.com>
2023-12-04 18:55:51 -0800

    Fix more linting errors in test_console/debug.

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,13 +1,12 @@
 import sys
 import os
-
-# Add the path to your project's root directory
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import unittest
 from main import UserMenu
 from unittest.mock import patch
 from io import StringIO
+
+# Add the path to your project's root directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 class TestStringMethods(unittest.TestCase):
@@ -15,15 +14,18 @@ class TestStringMethods(unittest.TestCase):
            side_effect=['1', '3', '3'])  # mock input order (edit, return to menu, quit)
     def test_menu_editor_back_quit(self, mock_input):
         with patch('sys.stdout',
-                   new_callable=StringIO) as mock_stdout:  # extract all print on screen into a string
+                   new_callable=StringIO) as mock_stdout:  # extract all print on screen into a
+            # string
             menu = UserMenu()
             menu.display_main_menu()
 
         printed_output = mock_stdout.getvalue()  # Get the printed output
 
         self.assertEqual(printed_output,
-                         "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player mode\n3. "
-                         "Quit\nYou are in the editor mode. Choices:\n    1. Create new campaign\n    "
+                         "Welcome to our text-based RPG maker!\n1. Editor mode\n"
+                         "2. Player mode\n3. "
+                         "Quit\nYou are in the editor mode. Choices:\n    1. Create new "
+                         "campaign\n    "
                          "2. Select existing campaign\n    3. Return to main menu\n"
                          "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player mode\n"
                          "3. Quit\n")
@@ -38,10 +40,13 @@ class TestStringMethods(unittest.TestCase):
         printed_output = mock_stdout.getvalue().strip()
 
         self.assertEqual(printed_output,
-                         "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player mode\n3. "
-                         "Quit\nYou are in the editor mode. Choices:\n    1. Create new campaign\n    "
+                         "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player"
+                         "mode\n3. "
+                         "Quit\nYou are in the editor mode. Choices:\n    1. Create new "
+                         "campaign\n    "
                          "2. Select existing campaign\n    3. Return to main menu\nInvalid choice, "
-                         "input should be a number corresponding to the list of choices.\nYou are in "
+                         "input should be a number corresponding to the list of choices.\nYou are "
+                         "in "
                          "the editor mode. Choices:\n    1. Create new campaign\n    2. Select "
                          "existing campaign\n    3. Return to main menu\n"
                          "Welcome to our text-based RPG maker!\n"
@@ -57,7 +62,8 @@ class TestStringMethods(unittest.TestCase):
         after_output = printed_output.strip()
 
         self.assertEqual(after_output,
-                         "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player mode\n3. Quit")
+                         "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player "
+                         "mode\n3. Quit")
 
 
 if __name__ == '__main__':
--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,14 +1,14 @@
 import sys
 import os
+from object_classes import Character, Player, DialogueEvent, Campaign
 
 # Add the path to your project's root directory
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from object_classes import Character, Player, DialogueEvent, Campaign
 
 # Property values for reuse in tests, note that these values are different from the default values
 # Test property values for the Player class
-test_ascii_art = "\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣬⡛⣿⣿⣿⣯⢻\n" \
+TEST_ASCII_ART = "\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣬⡛⣿⣿⣿⣯⢻\n" \
                  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢻⣿⣿⢟⣻⣿⣿⣿⣿⣿⣿⣮⡻⣿⣿⣧ \n" \
                  "⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣆⠻⡫⣢⠿⣿⣿⣿⣿⣿⣿⣿⣷⣜⢻⣿ \n" \
                  "⣿⣿⡏⣿⣿⣨⣝⠿⣿⣿⣿⣿⣿⢕⠸⣛⣩⣥⣄⣩⢝⣛⡿⠿⣿⣿⣆⢝ \n" \
@@ -21,94 +21,94 @@ test_ascii_art = "\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
                  "⣷⡝⣿⡞⣿⣿⣿⣿⣿⣿⣿⣿⡟⠋⠁⣠⣤⣤⣦⣽⣿⣿⣿⡿⠋⠘⣿⣿ \n" \
                  "⣿⣿⡹⣿⡼⣿⣿⣿⣿⣿⣿⣿⣧⡰⣿⣿⣿⣿⣿⣹⡿⠟⠉⡀⠄⠄⢿⣿ \n" \
                  "⣿⣿⣿⣽⣿⣼⣛⠿⠿⣿⣿⣿⣿⣿⣯⣿⠿⢟⣻⡽⢚⣤⡞⠄⠄⠄⢸⣿ \n"
-test_name = "John"
-test_base_hp = 20
-test_base_atk = 10
-test_base_spd = 2
-test_description = "custom character description"
-test_lvl = 2
-test_exp = 5
-test_exp_per_lvl_up = 50
-test_max_lvl = 15
-test_hp_mod = 10
-test_atk_mod = 3
-test_spd_mod = 5
+TEST_NAME = "John"
+TEST_BASE_HP = 20
+TEST_BASE_ATK = 10
+TEST_BASE_SPD = 2
+TEST_DESCRIPTION = "custom character description"
+TEST_LVL = 2
+TEST_EXP = 5
+TEST_EXP_PER_LVL_UP = 50
+TEST_MAX_LVL = 15
+TEST_HP_MOD = 10
+TEST_ATK_MOD = 3
+TEST_SPD_MOD = 5
 
 # Test property values for DialogueEvent class
-test_dialogue_event_id = 123
-test_dialogue_event_description = "dialogue event description"
-test_dialogue_event_choices = [111, 222, 333]
+TEST_DIALOGUE_EVENT_ID = 123
+TEST_DIALOGUE_EVENT_DESCRIPTION = "dialogue event description"
+TEST_DIALOGUE_EVENT_CHOICES = [111, 222, 333]
 
 # Test property values for campaign class
-test_campaign_name = "Test Campaign"
-test_campaign_short_desc = "campaign short description"
-test_campaign_events = {}
-test_campaign_player_list = []  # Note: add actual player classes
-test_campaign_npc_list = []  # Note: add actual NPC classes
-test_campaign_items = []  # TODO: add items when Item classes completed
+TEST_CAMPAIGN_NAME = "Test Campaign"
+TEST_CAMPAIGN_SHORT_DESC = "campaign short description"
+TEST_CAMPAIGN_EVENTS = {}
+TEST_CAMPAIGN_PLAYER_LIST = []  # Note: add actual player classes
+TEST_CAMPAIGN_NPC_LIST = []  # Note: add actual NPC classes
+TEST_CAMPAIGN_ITEMS = []  # TODO: add items when Item classes completed
 
 
 # Create a Character object with specific parameters
 def test_character_properties():
-    character = Character(name=test_name, base_hp=test_base_hp, base_atk=test_base_atk,
-                          base_spd=test_base_spd,
-                          description=test_description,
-                          ascii_art=test_ascii_art)
+    character = Character(name=TEST_NAME, base_hp=TEST_BASE_HP, base_atk=TEST_BASE_ATK,
+                          base_spd=TEST_BASE_SPD,
+                          description=TEST_DESCRIPTION,
+                          ascii_art=TEST_ASCII_ART)
 
     # Assert that the character's properties match the expected values
-    assert character.name == test_name
-    assert character.base_hp == test_base_hp
-    assert character.base_atk == test_base_atk
-    assert character.base_spd == test_base_spd
-    assert character.description == test_description
-    assert character.ascii_art == test_ascii_art
+    assert character.name == TEST_NAME
+    assert character.base_hp == TEST_BASE_HP
+    assert character.base_atk == TEST_BASE_ATK
+    assert character.base_spd == TEST_BASE_SPD
+    assert character.description == TEST_DESCRIPTION
+    assert character.ascii_art == TEST_ASCII_ART
 
 
 # Create a Player object with specific parameters
 def test_player_properties():
-    player = Player(name=test_name, base_hp=test_base_hp, base_atk=test_base_atk,
-                    base_spd=test_base_spd,
-                    description=test_description,
-                    ascii_art=test_ascii_art, lvl=test_lvl, exp=test_exp,
-                    exp_per_lvl_up=test_exp_per_lvl_up,
-                    max_lvl=test_max_lvl, hp_mod=test_hp_mod, atk_mod=test_atk_mod,
-                    spd_mod=test_spd_mod)
+    player = Player(name=TEST_NAME, base_hp=TEST_BASE_HP, base_atk=TEST_BASE_ATK,
+                    base_spd=TEST_BASE_SPD,
+                    description=TEST_DESCRIPTION,
+                    ascii_art=TEST_ASCII_ART, lvl=TEST_LVL, exp=TEST_EXP,
+                    exp_per_lvl_up=TEST_EXP_PER_LVL_UP,
+                    max_lvl=TEST_MAX_LVL, hp_mod=TEST_HP_MOD, atk_mod=TEST_ATK_MOD,
+                    spd_mod=TEST_SPD_MOD)
     # Assert that the object's inherited properties match the expected values
-    assert player.name == test_name
-    assert player.base_hp == test_base_hp
-    assert player.base_atk == test_base_atk
-    assert player.base_spd == test_base_spd
-    assert player.description == test_description
-    assert player.ascii_art == test_ascii_art
+    assert player.name == TEST_NAME
+    assert player.base_hp == TEST_BASE_HP
+    assert player.base_atk == TEST_BASE_ATK
+    assert player.base_spd == TEST_BASE_SPD
+    assert player.description == TEST_DESCRIPTION
+    assert player.ascii_art == TEST_ASCII_ART
     # Assert that the object's custom properties match the expected values
-    assert player.lvl == test_lvl
-    assert player.exp == test_exp
-    assert player.exp_per_lvl_up == test_exp_per_lvl_up
-    assert player.max_lvl == test_max_lvl
-    assert player.hp_mod == test_hp_mod
-    assert player.atk_mod == test_atk_mod
-    assert player.spd_mod == test_spd_mod
+    assert player.lvl == TEST_LVL
+    assert player.exp == TEST_EXP
+    assert player.exp_per_lvl_up == TEST_EXP_PER_LVL_UP
+    assert player.max_lvl == TEST_MAX_LVL
+    assert player.hp_mod == TEST_HP_MOD
+    assert player.atk_mod == TEST_ATK_MOD
+    assert player.spd_mod == TEST_SPD_MOD
 
 
 # Create a DialogueEvent object with specific parameters
 def test_dialogue_event_properties():
-    dialogue_event = DialogueEvent(event_id=test_dialogue_event_id,
-                                   description=test_dialogue_event_description,
-                                   choices=test_dialogue_event_choices)
+    dialogue_event = DialogueEvent(event_id=TEST_DIALOGUE_EVENT_ID,
+                                   description=TEST_DIALOGUE_EVENT_DESCRIPTION,
+                                   choices=TEST_DIALOGUE_EVENT_CHOICES)
     # Assert that the object's custom properties match the expected values
-    assert dialogue_event.event_id == test_dialogue_event_id
-    assert dialogue_event.description == test_dialogue_event_description
-    assert dialogue_event.choices == test_dialogue_event_choices
+    assert dialogue_event.event_id == TEST_DIALOGUE_EVENT_ID
+    assert dialogue_event.description == TEST_DIALOGUE_EVENT_DESCRIPTION
+    assert dialogue_event.choices == TEST_DIALOGUE_EVENT_CHOICES
 
 
 # Create a Campaign object with specific parameters and test them
 def test_campaign_properties():
-    campaign = Campaign(test_campaign_name, test_campaign_short_desc, test_campaign_events,
-                        test_campaign_player_list,
-                        test_campaign_npc_list, test_campaign_items)
-    assert campaign.name == test_campaign_name
-    assert campaign.short_desc == test_campaign_short_desc
-    assert campaign.events == test_campaign_events
-    assert campaign.player_list == test_campaign_player_list
-    assert campaign.npc_list == test_campaign_npc_list
+    campaign = Campaign(TEST_CAMPAIGN_NAME, TEST_CAMPAIGN_SHORT_DESC, TEST_CAMPAIGN_EVENTS,
+                        TEST_CAMPAIGN_PLAYER_LIST,
+                        TEST_CAMPAIGN_NPC_LIST, TEST_CAMPAIGN_ITEMS)
+    assert campaign.name == TEST_CAMPAIGN_NAME
+    assert campaign.short_desc == TEST_CAMPAIGN_SHORT_DESC
+    assert campaign.events == TEST_CAMPAIGN_EVENTS
+    assert campaign.player_list == TEST_CAMPAIGN_PLAYER_LIST
+    assert campaign.npc_list == TEST_CAMPAIGN_NPC_LIST
     # assert campaign._items_list == test_campaign_items
