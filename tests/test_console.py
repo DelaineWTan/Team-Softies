22f0f74211467aa8b4833d7fcc2226b670614678
Delaine <delainewtan@gmail.com>
2023-12-04 19:00:57 -0800

    Fix unused vars error for test_console.py.

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 class TestStringMethods(unittest.TestCase):
     @patch('builtins.input',
            side_effect=['1', '3', '3'])  # mock input order (edit, return to menu, quit)
-    def test_menu_editor_back_quit(self, mock_input):
+    def test_menu_editor_back_quit(self, _):
         with patch('sys.stdout',
                    new_callable=StringIO) as mock_stdout:  # extract all print on screen into a
             # string
@@ -32,7 +32,7 @@ class TestStringMethods(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['1', 'no choice', '3',
                                           '3'])  # mock input order (edit, invalid, return, quit)
-    def test_menu_editor_invalid_option(self, mock_input):
+    def test_menu_editor_invalid_option(self, _):
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             menu = UserMenu()
             menu.display_main_menu()
@@ -40,7 +40,7 @@ class TestStringMethods(unittest.TestCase):
         printed_output = mock_stdout.getvalue().strip()
 
         self.assertEqual(printed_output,
-                         "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player"
+                         "Welcome to our text-based RPG maker!\n1. Editor mode\n2. Player "
                          "mode\n3. "
                          "Quit\nYou are in the editor mode. Choices:\n    1. Create new "
                          "campaign\n    "
@@ -53,7 +53,7 @@ class TestStringMethods(unittest.TestCase):
                          "1. Editor mode\n2. Player mode\n3. Quit")
 
     @patch('builtins.input', return_value='3')
-    def test_quit_game_immediately(self, mock_input):
+    def test_quit_game_immediately(self, _):
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             menu = UserMenu()
             menu.display_main_menu()
